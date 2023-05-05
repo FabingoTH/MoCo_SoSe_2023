059830c4a4ba699ad7223cfc859581df871b5556
Fabian Ngo <fabian.ngo@smail.th-koeln.de>
2023-05-05 16:37:27 +0200

    add multiple Screens

--- app/src/main/java/com/example/marboles/MainActivity.kt
+++ app/src/main/java/com/example/marboles/MainActivity.kt
@@ -1,58 +1,65 @@
 package com.example.marboles
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ShoppingCart
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.marboles.ui.theme.MarbolesTheme
-
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import android.content.Context
-import android.app.Activity
-import android.content.ContextWrapper
-import android.content.pm.ActivityInfo
-import android.graphics.BlurMaskFilter
-import android.graphics.Paint
-import android.graphics.Paint.Align
-import android.graphics.RectF
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.*
-import androidx.compose.ui.text.style.TextAlign
-import org.w3c.dom.Text
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
-            // LOCK SCREEN LANDSCAPE MODE
+            // LOCK SCREEN TO LANDSCAPE MODE
             LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 
             MarbolesTheme {
+
                 WoodImage()
                 //Homescreen()
-                ScoreScreen()
+                //ScoreScreen()
+                //LevelChoiceScreen()
+                PauseScreen()
             }
         }
     }
@@ -88,11 +95,12 @@ fun Homescreen() {
             TitleText(title = "Marboles")
             Row(
                 modifier = Modifier
-                    .fillMaxWidth(0.5f),
+                    .fillMaxWidth(0.5f)
+                    .padding(60.dp, 0.dp),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-               SimpleButton(label = "Start")
-               SimpleButton(label = "Scores")
+               SimpleButton(label = "Play")
+               SimpleButton(label = "Level")
             }
         }
     }
@@ -105,7 +113,7 @@ fun TitleText(title : String) {
     Box(
         modifier = Modifier
             .background(Color.White, RoundedCornerShape(100.dp))
-            .padding(50.dp, 20.dp)
+            .padding(50.dp, 10.dp)
     ) {
         Text(
             title.uppercase(),
@@ -113,7 +121,7 @@ fun TitleText(title : String) {
                 color = Color.Black,
                 fontSize = 50.sp,
                 textAlign = TextAlign.Center,
-                letterSpacing = 10.sp
+                letterSpacing = 20.sp
             )
         )
     }
@@ -149,21 +157,21 @@ fun ScoreScreen () {
             Card(
                 modifier = Modifier
                     .fillMaxWidth(0.8f)
-                    .fillMaxHeight( 0.85f),
+                    .fillMaxHeight(0.85f),
                 shape = RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp)
             ) {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(100.dp,30.dp),
+                        .padding(100.dp, 30.dp),
                 ) {
                     Row(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(0.dp,30.dp,0.dp,50.dp),
+                            .padding(0.dp, 30.dp, 0.dp, 50.dp),
                         horizontalArrangement = Arrangement.Center
                     ) {
-                        Text(text = "HIGHSCORES", color = Color.Black, fontSize = 40.sp, letterSpacing = 10.sp)
+                        MenuTitle(label = "Highscores")
                     }
 
                     Column(
@@ -192,6 +200,169 @@ fun ScoreEntry(spielerName: String, score: String) {
         Text(text = score.toString(), color = Color.Black, fontSize = 30.sp)
     }
 }
+
+// Levelauswahl
+@Composable
+fun LevelChoiceScreen () {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(0.85f),
+                shape = RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(100.dp, 30.dp),
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 30.dp, 0.dp, 50.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        MenuTitle(label = "Level")
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+
+                            buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    color = Color.Black,
+                                )
+                            ) {
+                                append("1 ")
+                            }
+                            append("2 3 4 5 6")
+
+                        },
+                            fontSize = 30.sp,
+                            color = Color.Gray,
+                            letterSpacing = 15.sp,
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MenuTitle(label : String) {
+    Text(text = label.uppercase(), color = Color.Black, fontSize = 40.sp, letterSpacing = 10.sp)
+}
+
+
+@Composable
+fun PauseScreen() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(50.dp, 15.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row() {
+                    Text(text = "ll", fontSize = 20.sp)
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    Text(text = "Level", fontSize = 20.sp)
+                }
+                Text(text = "Time: 00:49", fontSize = 20.sp)
+            }
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight(1f),
+                shape = RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(100.dp, 30.dp),
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 30.dp, 0.dp, 40.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        MenuTitle(label = "Pause")
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                      Column() {
+                          Row(
+                              Modifier.fillMaxWidth(0.4f),
+                              horizontalArrangement = Arrangement.SpaceBetween,
+                              verticalAlignment =  Alignment.CenterVertically
+
+                          ) {
+                              Text(
+                                  modifier = Modifier
+                                      .padding(0.dp, 0.dp, 30.dp,0.dp),
+                                  text = "SFX", fontSize = 30.sp)
+                              val checkedState = remember { mutableStateOf(true) }
+                              Switch(
+                                  checked = checkedState.value,
+                                  onCheckedChange = { checkedState.value = it }
+                              )
+                          }
+                          Row(
+                              Modifier.fillMaxWidth(0.4f),
+                              horizontalArrangement = Arrangement.SpaceBetween,
+                              verticalAlignment =  Alignment.CenterVertically
+
+                          ) {
+                              Text(
+                                  modifier = Modifier
+                                      .padding(0.dp, 0.dp, 30.dp,0.dp),
+                                  text = "Music", fontSize = 30.sp)
+                              val checkedState = remember { mutableStateOf(true) }
+                              Switch(
+                                  checked = checkedState.value,
+                                  onCheckedChange = { checkedState.value = it }
+                              )
+                          }
+                          //Text(text = "Exit", fontSize = 30.sp)
+                      }
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 @Composable
 fun LockScreenOrientation(orientation: Int) {
