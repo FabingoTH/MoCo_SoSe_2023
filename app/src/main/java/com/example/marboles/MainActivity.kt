0aeb3eb17fe810fc5b749989a0bc4928f40e3b40
Fabian Ngo <fabian.ngo@smail.th-koeln.de>
2023-05-05 14:26:10 +0200

    UI Wireframes

---
+++ app/src/main/java/com/example/marboles/MainActivity.kt
@@ -0,0 +1,217 @@
+package com.example.marboles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.marboles.ui.theme.MarbolesTheme
+
+import androidx.compose.runtime.Composable
+import android.content.Context
+import android.app.Activity
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
+import android.graphics.BlurMaskFilter
+import android.graphics.Paint
+import android.graphics.Paint.Align
+import android.graphics.RectF
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.*
+import androidx.compose.ui.text.style.TextAlign
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            // LOCK SCREEN LANDSCAPE MODE
+            LockScreenOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+            MarbolesTheme {
+                WoodImage()
+                //    Homescreen()
+                ScoreScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun WoodImage() {
+    Image(
+        modifier = Modifier.fillMaxWidth(),
+        painter = painterResource(id = R.drawable.wood),
+        contentDescription = "Holzhintergrund",
+        contentScale = ContentScale.Crop
+    )
+}
+
+// HOME
+@Composable
+fun Homescreen() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(0.7f)
+                .background(Color.Red),
+            verticalArrangement = Arrangement.SpaceAround,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleText(title = "Marboles")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .background(Color.Blue),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+               SimpleButton(label = "Start")
+               SimpleButton(label = "Scores")
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun TitleText(title : String) {
+    Box(
+        modifier = Modifier
+            .background(Color.White, RoundedCornerShape(100.dp))
+            .padding(50.dp, 12.dp)
+    ) {
+        Text(
+            title.uppercase(),
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 50.sp,
+                textAlign = TextAlign.Center,
+                letterSpacing = 10.sp
+            )
+        )
+    }
+}
+
+@Composable
+fun SimpleButton(label : String){
+    Button(
+        onClick = {},
+        elevation = ButtonDefaults.elevation(0.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = Color.Transparent))
+
+    {
+        Text(text = label, color = Color.Black, fontSize = 30.sp)
+    }
+}
+
+// SCORE
+
+@Composable
+fun ScoreScreen () {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .fillMaxHeight( 0.8f),
+                shape = RoundedCornerShape(30.dp, 30.dp, 0.dp, 0.dp)
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(50.dp,30.dp),
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp,0.dp,0.dp,30.dp),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "HIGHSCORES", color = Color.Black, fontSize = 50.sp, letterSpacing = 10.sp)
+                    }
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ) {
+                        ScoreEntry(spielerName = "Anouk", score = 1000)
+                        ScoreEntry(spielerName = "Phit", score = 800)
+                        ScoreEntry(spielerName = "Fabian", score = 700)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ScoreEntry(spielerName: String, score: Int) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = spielerName, color = Color.Black, fontSize = 30.sp)
+        Text(text = score.toString(), color = Color.Black, fontSize = 30.sp)
+    }
+}
+
+@Composable
+fun LockScreenOrientation(orientation: Int) {
+    val context = LocalContext.current
+
+    DisposableEffect(orientation) {
+        val activity = context.findActivity() ?: return@DisposableEffect onDispose {}
+        val originalOrientation = activity.requestedOrientation
+        activity.requestedOrientation = orientation
+        onDispose {
+            // restore original orientation when view disappears
+            activity.requestedOrientation = originalOrientation
+        }
+    }
+}
+
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
