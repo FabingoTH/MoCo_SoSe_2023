afb9b235b0c2d948b6dcb3a80c294ba032506e8e
phkh25 <phkh25@gmail.com>
2023-06-24 12:32:05 +0200

    changed parameters for Goal

--- app/src/main/java/com/example/marboles/gamemanager/Goal.kt
+++ app/src/main/java/com/example/marboles/gamemanager/Goal.kt
@@ -4,15 +4,19 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.translate
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
 
 @Composable
 fun Goal(){
-    Canvas(modifier = Modifier.fillMaxSize()) {
-        translate(left = 250f, top = 200f) {
-            drawCircle(Color.DarkGray, radius = 30.dp.toPx())
-        }
+    Canvas( modifier = Modifier ) {
+        drawCircle(
+            color = Color.DarkGray, radius =60f, center=
+            Offset(x = 160.dp.toPx(), y = 135.dp.toPx()))
     }
 }
