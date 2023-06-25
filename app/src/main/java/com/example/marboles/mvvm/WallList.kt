3766af4ea5251e8a070bd2eac4b25506e5d4d7bb
phkh <phkh@pop-os.localdomain>
2023-06-25 19:48:48 +0200

    bugfix: ball stuck at corners of squares

--- app/src/main/java/com/example/marboles/mvvm/WallList.kt
+++ app/src/main/java/com/example/marboles/mvvm/WallList.kt
@@ -1,11 +1,6 @@
 package com.example.marboles.mvvm
 
 import androidx.compose.ui.unit.dp
-
-// Ich hab die Wandliste mal zu einer globalen Variable gemacht damit man im Model und im
-// View drauf zugreifen kann (Ich weiß nicht ob das von der Architektur her so stimmt? Aber
-// soll erst mal funktionieren. Ich checke damit die Kollisionen und zeichne die Wände im View)
-
 class Wall(
     val wallLeftX: Float,
     val wallRightX: Float,
@@ -24,13 +19,10 @@ class Wall(
 //            wallRight ergibt sich aus dem x Wert von topLeft + width des Rechtecks
 //            wallTop aus dem y-Wert von topLeft + height des Rechtecks
 
-val wall0 = Wall(40f, 90f, 160f, 60f) // OG Wand
-val wall1 = Wall(40f, 290f, 60f, 10f)
-val wall2 = Wall(-200f, 90f, 10f, -40f)
+val wall0 = Wall(40f, 90f, 250f, -100f) // OG Wand
+val wall1 = Wall(42f, 290f, 60f, 10f)
+val wall2 = Wall(-200f, 88f, 10f, -40f)
 val wall3 = Wall(-160f, -110f, 120f, 70f)
 val wall4 = Wall(0f, 50f, 0f, 50f) // Quadrat als Referenzpunkt in der Mitte des Screens
 
 var walls = listOf(wall0, wall1, wall2, wall3)
-
-
-// Übergang von einem Rechteck zum anderen hakelig
